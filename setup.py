fe8f45dcbe01f1e867c4f4f388d4da577af5f611
hong.zou5 <admin@proyy.com>
2025-11-19 22:11:11 +0800

    build: 更新版本号至1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="pysslvpn",
-    version="1.0.0",
+    version="1.0.1",
     author="PySslvpn Developer",
     author_email="admin@proyy.com",
     description="A pure Python SSL VPN client implementation",
